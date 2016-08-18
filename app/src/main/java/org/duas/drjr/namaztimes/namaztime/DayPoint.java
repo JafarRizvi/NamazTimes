560720ad52c8e8c87a62a437cd4c74dd2f25cc6e
Jafar Rizvi <jafarrizvi@hotmail.com>
2016-08-18 15:50:18 +0500

    Add Day Point Enumeration

---
+++ app/src/main/java/org/duas/drjr/namaztimes/namaztime/DayPoint.java
@@ -0,0 +1,18 @@
+package org.duas.drjr.namaztimes.namaztime;
+
+/**
+ * Created by DrJafarRizvi on 18-Aug-2016.
+ */
+
+public enum DayPoint {
+    Imsak,
+    Fajr,
+    Sunrise,
+    Dhuhr,
+    Asr,
+    Sunset,
+    Maghrib,
+    Isha,
+    Midnight
+}
+
