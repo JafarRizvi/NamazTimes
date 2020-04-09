3d87cf3d7ff99a7808e9d46f46af16ee139e8650
Dr. Jafar Rizvi <jafarrizvi@hotmail.com>
2020-04-09 16:57:03 +0500

    Added CityDao.java

---
+++ app/src/main/java/org/duas/drjr/namaztimes/data/CityDao.java
@@ -0,0 +1,24 @@
+package org.duas.drjr.namaztimes.data;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+@Dao
+public interface CityDao {
+    @Query("SELECT * FROM cities ORDER BY city")
+    LiveData<List<City>> getCities();
+
+    @Query("SELECT * FROM cities WHERE province = :province ORDER BY city")
+    LiveData<List<City>> getCitiesForProvince(String province);
+
+    @Query("SELECT * FROM cities WHERE id = :cityId")
+    LiveData<City> getCity(int cityId);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(List<City> cities);
+}
